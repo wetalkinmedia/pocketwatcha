45b3403489201877ddb96a3e8b3a34d1d2135336
wetalkinmedia <wetalkinmedia@gmail.com>
2025-10-25 22:18:44 -0400

    Delete project-bolt-sb1-fsnrnatt (1)/project directory

--- project-bolt-sb1-fsnrnatt (1)/project/package-project.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Packaging project files...
-tar -czf project-backup.tar.gz --exclude=node_modules --exclude=.git --exclude=dist --exclude=*.tar.gz .
-echo Done! File created: project-backup.tar.gz
-pause
